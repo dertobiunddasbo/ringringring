62b14deffb02a868881d9c7d279a202ac04bb070
dertobiunddasbo <tobias.abt@bsp-media.de>
2025-06-24 14:11:03 +0200

    animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'fade-in': 'fadeIn 1.2s ease-out forwards',
+        'slide-up': 'slideUp 1s ease-out forwards',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+        slideUp: {
+          '0%': { opacity: 0, transform: 'translateY(20px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+      },
+    },
   },
   plugins: [],
-};
+}
